825e9df8fc35cb678ee8e108350cf9137ef5876b
Alice Tries <alycecil@gmail.com>
2021-08-29 10:51:39 -0400

    Add shield generators to the titan

--- src/main/java/com/github/alycecil/hullmods/ShieldLostTech.java
+++ src/main/java/com/github/alycecil/hullmods/ShieldLostTech.java
@@ -3,6 +3,7 @@ package com.github.alycecil.hullmods;
 import com.fs.starfarer.api.combat.MutableShipStatsAPI;
 import com.fs.starfarer.api.combat.ShieldAPI;
 import com.fs.starfarer.api.combat.ShipAPI;
+import com.fs.starfarer.api.combat.ShipwideAIFlags;
 import com.github.alycecil.hullmods.abstracts.CommonCombatHullmod;
 
 import java.awt.*;
@@ -31,17 +32,39 @@ public class ShieldLostTech extends CommonCombatHullmod {
     }
 
     @Override
+    public void advanceInCombat(ShipAPI ship, float amount) {
+        super.advanceInCombat(ship, amount);
+
+        if (ship == null || !ship.isAlive()) return;
+
+        ShieldAPI shield = ship.getShield();
+        if (shield == null) return;
+
+        setColor(shield);
+    }
+
+    @Override
     protected void actionForShip(ShipAPI ship) {
         if (ship == null) return;
 
         ShieldAPI shield = ship.getShield();
         if (shield == null) return;
 
+        setColor(shield);
+
+        if (ship.areSignificantEnemiesInRange()
+        ) {
+            if (!ship.getFluxTracker().isOverloadedOrVenting() && ship.getFluxTracker().getFluxLevel() < 0.7f) {
+                shield.toggleOn();
+                ship.getAIFlags().setFlag(ShipwideAIFlags.AIFlags.KEEP_SHIELDS_ON, 1f);
+            }
+        } else {
+            ship.getAIFlags().removeFlag(ShipwideAIFlags.AIFlags.KEEP_SHIELDS_ON);
+        }
+    }
+
+    protected void setColor(ShieldAPI shield) {
         shield.setInnerColor(Color.CYAN);
         shield.setRingColor(Color.YELLOW);
-
-        if(ship.areSignificantEnemiesInRange() && !ship.getFluxTracker().isOverloadedOrVenting()){
-            shield.toggleOn();
-        }
     }
 }
